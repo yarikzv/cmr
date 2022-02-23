a39ded90bbe583b677e8c8c3baf850eb78d35afc
Yaroslav Zvolinskiy <yzv.yaroslav@gmail.com>
2022-02-23 10:38:06 +0200

    Add new Driver Tab with list of all drivers

--- src/main/java/dev/zvolinskiy/cmr/controller/DriverController.java
+++ src/main/java/dev/zvolinskiy/cmr/controller/DriverController.java
@@ -4,13 +4,17 @@ import dev.zvolinskiy.cmr.entity.Driver;
 import dev.zvolinskiy.cmr.entity.Passport;
 import dev.zvolinskiy.cmr.service.DriverService;
 import dev.zvolinskiy.cmr.service.PassportService;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,6 +48,48 @@ public class DriverController {
     public TextField tfDriverLastNameSearch;
     @FXML
     public Button searchDriverByLastName;
+    @FXML
+    public TextField tfDriverPassportSearch;
+    @FXML
+    public Button searchDriverByPassportNumber;
+    @FXML
+    public TableColumn<Driver, String> colLastName;
+    @FXML
+    public TableColumn<Driver, String> colFirstName;
+    @FXML
+    public TableColumn<Driver, String> colMiddleName;
+    @FXML
+    public TableColumn<Driver, String> colPassportNumber;
+    @FXML
+    public TableColumn<Driver, String> colPassportIssue;
+    @FXML
+    public TableColumn<Driver, String> colPassportDate;
+    @FXML
+    public TableColumn<Driver, String> colTruck;
+    @FXML
+    public TableColumn<Driver, String> colTrailer;
+    @FXML
+    public TableView<Driver> searchResultTable;
+    @FXML
+    public Button getDriversListButton;
+    @FXML
+    public TableView<Driver> driverListTable;
+    @FXML
+    public TableColumn<Driver, String> listLastName;
+    @FXML
+    public TableColumn<Driver, String> listFirstName;
+    @FXML
+    public TableColumn<Driver, String> listMiddleName;
+    @FXML
+    public TableColumn<Driver, String> listPassportNumber;
+    @FXML
+    public TableColumn<Driver, String> listPassportIssue;
+    @FXML
+    public TableColumn<Driver, String> listPassportDate;
+    @FXML
+    public TableColumn<Driver, String> listTruck;
+    @FXML
+    public TableColumn<Driver, String> listTrailer;
 
     public void saveDriverAction() {
         String driverPassportSeries = tfDriverPassportSeries.getText();
@@ -96,11 +142,70 @@ public class DriverController {
         tfDriverTrailer.clear();
     }
 
-    public Driver searchDriverByLastNameAction() {
-
-        return null;
+    public void searchDriverByLastNameAction() {
+        List<Driver> driversByLastName = driverService.findDriverByLastName(tfDriverLastNameSearch.getText());
+        fillTableBySearchResult(driversByLastName,
+                colLastName,
+                colFirstName,
+                colMiddleName,
+                colPassportNumber,
+                colPassportIssue,
+                colPassportDate,
+                colTruck,
+                colTrailer,
+                searchResultTable);
+        tfDriverLastNameSearch.clear();
     }
 
     public void searchDriverByPassportNumberAction() {
+//        TODO Change type of search result by passport number
+        List<Driver> driversByPassport = List.of(driverService.findDriverByPassport(tfDriverPassportSearch.getText()));
+        fillTableBySearchResult(driversByPassport,
+                colLastName,
+                colFirstName,
+                colMiddleName,
+                colPassportNumber,
+                colPassportIssue,
+                colPassportDate,
+                colTruck,
+                colTrailer,
+                searchResultTable);
+        tfDriverPassportSearch.clear();
+    }
+
+    public void getDriversListAction() {
+        List<Driver> drivers = driverService.findAllDrivers();
+        fillTableBySearchResult(drivers,
+                listLastName,
+                listFirstName,
+                listMiddleName,
+                listPassportNumber,
+                listPassportIssue,
+                listPassportDate,
+                listTruck,
+                listTrailer,
+                driverListTable);
+    }
+
+    private void fillTableBySearchResult(List<Driver> drivers,
+                                         TableColumn<Driver, String> lastName,
+                                         TableColumn<Driver, String> firstName,
+                                         TableColumn<Driver, String> middleName,
+                                         TableColumn<Driver, String> passportNumber,
+                                         TableColumn<Driver, String> passportIssue,
+                                         TableColumn<Driver, String> passportDate,
+                                         TableColumn<Driver, String> truck,
+                                         TableColumn<Driver, String> trailer,
+                                         TableView<Driver> table
+    ) {
+        lastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        middleName.setCellValueFactory(new PropertyValueFactory<>("middleName"));
+        passportNumber.setCellValueFactory(number -> new SimpleStringProperty(number.getValue().getPassport().getNumber()));
+        passportIssue.setCellValueFactory(issue -> new SimpleStringProperty(issue.getValue().getPassport().getIssue()));
+        passportDate.setCellValueFactory(date -> new SimpleStringProperty(date.getValue().getPassport().getDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+        truck.setCellValueFactory(new PropertyValueFactory<>("truck"));
+        trailer.setCellValueFactory(new PropertyValueFactory<>("trailer"));
+        table.getItems().setAll(drivers);
     }
 }
