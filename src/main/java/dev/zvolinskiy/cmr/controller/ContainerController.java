6a9696a08c1acb547bd09dc6a964c0a7ef7fd37b
Yaroslav Zvolinskiy <yzv.yaroslav@gmail.com>
2022-03-09 23:09:19 +0200

    Validate input in ContainerController

--- src/main/java/dev/zvolinskiy/cmr/controller/ContainerController.java
+++ src/main/java/dev/zvolinskiy/cmr/controller/ContainerController.java
@@ -3,17 +3,21 @@ package dev.zvolinskiy.cmr.controller;
 import dev.zvolinskiy.cmr.entity.Container;
 import dev.zvolinskiy.cmr.service.ContainerService;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
+import java.net.URL;
 import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 @Controller
 @RequiredArgsConstructor
-public class ContainerController {
+public class ContainerController implements Initializable {
     private final ContainerService containerService;
 
     @FXML
@@ -36,26 +40,59 @@ public class ContainerController {
     public Button getContainerListButton;
     @FXML
     public TableView<Container> containerListTable;
+    public Button closeButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tfContainerNumber.focusedProperty().addListener((arg0, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!tfContainerNumber.getText().matches("[A-Za-z]{4}\\d{7}")) {
+                    tfContainerNumber.clear();
+                    Alert alert = new Alert(Alert.AlertType.ERROR,
+                            "Введите правильный номер контейнера в формате ХХХХ0000000",
+                            ButtonType.OK);
+                    alert.setTitle("Ошибка!");
+                    alert.setHeaderText("Ошибка!");
+                    alert.showAndWait().ifPresent(rs -> {
+                        if (rs == ButtonType.OK) alert.close();
+                    });
+                }
+            }
+        });
+    }
 
     public void saveContainerAction() {
-        String containerNumber = tfContainerNumber.getText();
+        String containerNumber = tfContainerNumber.getText().toUpperCase();
         String containerType = tfContainerType.getText();
+        if (!containerNumber.equals("") && !Objects.equals(containerType, "")) {
+            Container container = Container.builder()
+                    .number(containerNumber)
+                    .type(containerType)
+                    .build();
+            containerService.saveContainer(container);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                    "Контейнер " +
+                            container.getNumber() +
+                            " успешно сохранен в базу данных!",
+                    ButtonType.OK);
+            alert.setTitle("Поздравляю!");
+            alert.setHeaderText("Поздравляю!");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) alert.close();
+            });
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR,
+                    "Заполните поля!",
+                    ButtonType.OK);
+            alert.setTitle("Ошибка!");
+            alert.setHeaderText("Ошибка!");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) alert.close();
+            });
+        }
+    }
 
-        Container container = Container.builder()
-                .number(containerNumber)
-                .type(containerType)
-                .build();
-
-        containerService.saveContainer(container);
-
-        Alert alert = new Alert(Alert.AlertType.INFORMATION,
-                "Контейнер " +
-                        container.getNumber() +
-                        " успешно сохранен в базу данных!",
-                ButtonType.OK);
-        alert.showAndWait().ifPresent(rs -> {
-            if (rs == ButtonType.OK) alert.close();
-        });
+    public void closeButtonAction() {
         containerAnchorPane.getScene().getWindow().hide();
     }
 
